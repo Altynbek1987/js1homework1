5dbdc3a28dc76d8dea4f9fbeabb7f454afd82f44
Altynbek1987 <hyundai8788@gmail.com>
2022-09-06 19:17:37 +0600

    js1homework1

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const color = prompt('Input color');
+const mass = ['Red','Yellow','Green'];
+
+
+switch (color) {
+    case 'Red':
+        alert('Stop');
+        break;
+    case 'Yellow':
+        alert('Expectation');
+        break;
+    case 'Green':
+        alert('Go');
+        break;
+    default:
+        alert('Color is not too late');
+        break;
+}
