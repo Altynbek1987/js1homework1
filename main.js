6460c5894b5e6135973c9aca9245c417347b1e0e
Altynbek1987 <hyundai8788@gmail.com>
2022-09-07 14:58:18 +0600

    Сделал вторую и третью задачу

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
+// # One task
 
-const color = prompt('Input color');
+// const color = prompt('Input color');
 
-switch (color) {
-    case 'Red','red':
-        alert('Stop');
-        break;
-    case 'Yellow','yellow':
-        alert('Expectation');
-        break;
-    case 'Green','green':
-        alert('Go');
-        break;
-    default:
-        alert('Color is not too late');
-        break;
+// switch (color) {
+//     case 'Red','red':
+//         alert('Stop');
+//         break;
+//     case 'Yellow','yellow':
+//         alert('Expectation');
+//         break;
+//     case 'Green','green':
+//         alert('Go');
+//         break;
+//     default:
+//         alert('Color is not too late');
+//         break;
+// }
+
+//==================================================
+
+// # Two task
+
+// const firstName = prompt('Введите имя:');
+// const lateName = prompt('Введите фамилию:');
+
+// alert('Hello ' + lateName + ' ' + firstName);
+
+//=======================================================
+
+// Three task
+
+const firstNumber = prompt('Введите первое число:');
+const secondNumber = prompt('Введите второе число:');
+
+if (firstNumber > secondNumber) {
+    alert(firstNumber + ' больше чем -> ' + secondNumber);   
+}
+else if(firstNumber < secondNumber){
+    alert(secondNumber + ' больше чем -> ' + firstNumber); 
+}
+else if(firstNumber == secondNumber){
+    alert(secondNumber + ' равные числа ' + firstNumber); 
 }
 
-
-
-
+//===================================================================
